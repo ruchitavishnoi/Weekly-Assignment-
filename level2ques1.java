c5910b4732ceba4aebb92c5f982c9d28cb888eee
ruchitavishnoi <vishnoiruchita@gmail.com>
2026-03-10 23:37:56 +0530

    OddEvenChecker

--- level2ques1.java
+++ level2ques1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class OddEvenChecker {
+public class level2ques1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
