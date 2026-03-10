f6147a1c996b8678328daa3a6493daf94743684e
ruchitavishnoi <vishnoiruchita@gmail.com>
2026-03-10 23:36:24 +0530

    smallest number check

--- level1ques2.java
+++ level1ques2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SmallestNumberCheck {
+public class level1ques2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
