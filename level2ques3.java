33a644cbd0becfedf5d5a062897c9e4abcf49f84
ruchitavishnoi <vishnoiruchita@gmail.com>
2026-03-10 23:38:47 +0530

    MultiplicationTable

--- level2ques3.java
+++ level2ques3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MultiplicationTable {
+public class level2ques3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
